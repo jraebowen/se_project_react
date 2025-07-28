ed33f03067b876b37dd67b0a3201a5b64c70379c
Jaimie Bowen <jraebowen@gmail.com>
2025-07-28 14:37:50 -0700

    initial commit. Added file structure and import/export statements

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import ItemModal from "../ItemModal/ItemModal";
+import WeatherCard from "../WeatherCard/WeatherCard"; //Is this needed since it's a child of main?
+import ItemCard from "../ItemCard/ItemCard"; //Is this needed since it's a child of main?
+
+export default App;
