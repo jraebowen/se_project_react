a28314d83d32db09976f6c841752913b84d7bfbe
Jaimie Bowen <jraebowen@gmail.com>
2025-07-29 12:34:47 -0700

    set up header and styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,23 @@ import { useState } from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
-import Footer from "../Footer/Footer";
-import ModalWithForm from "../ModalWithForm/ModalWithForm";
-import ItemModal from "../ItemModal/ItemModal";
-import WeatherCard from "../WeatherCard/WeatherCard"; //Is this needed since it's a child of main?
-import ItemCard from "../ItemCard/ItemCard"; //Is this needed since it's a child of main?
+// import Footer from "../Footer/Footer";
+// import ModalWithForm from "../ModalWithForm/ModalWithForm";
+// import ItemModal from "../ItemModal/ItemModal";
+// import WeatherCard from "../WeatherCard/WeatherCard"; //Is this needed since it's a child of main?
+// import ItemCard from "../ItemCard/ItemCard"; //Is this needed since it's a child of main?
+
+function App() {
+  return (
+    <>
+      <div className="page">
+        <div className="page__content">
+          <Header></Header>
+          <Main></Main>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,28 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.webp";
+
+function Header() {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="header__container_logo-date">
+          <img className="header__logo" src={logo} alt="WTWR logo"></img>
+          <p className="header__date-location"></p>
+        </div>
+        <div className="header__container_profile-details">
+          <button className="header__button">+ Add Clothes</button>
+          <p className="header__name">Terrence Tegegne</p>
+          <img src={avatar} alt="profile picture" className="header__avatar" />
+        </div>
+      </div>
+    </header>
+  );
+}
+
 export default Header;
