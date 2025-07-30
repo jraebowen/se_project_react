c42043abd5789705ea5332c8aa07be2bbbc06a4e
Jaimie Bowen <jraebowen@gmail.com>
2025-07-30 08:48:45 -0700

    set up styling for itemcards

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,1 +1,15 @@
+import "./ItemCard.css";
+import tshirt from "../../assets/t-shirt.png";
+
+function ItemCard() {
+  return (
+    <>
+      <section className="item-card">
+        <p className="item-card__title">T-Shirt</p>
+        <img src={tshirt} alt="" className="item-card__image" />
+      </section>
+    </>
+  );
+}
+
 export default ItemCard;
