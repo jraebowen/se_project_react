8ecad9ca9805a5b6c33e15303b7288749eccd9d1
Jaimie Bowen <jraebowen@gmail.com>
2025-08-03 14:48:03 -0600

    refactor modal with form for usability

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,14 +15,14 @@ function App() {
     location: "",
   });
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [activeModal, setActiveModal] = useState("");
 
   const handleModalOpen = () => {
-    setIsModalOpen(true);
+    setActiveModal(true);
   };
 
   const handleModalClose = () => {
-    setIsModalOpen(false);
+    setActiveModal(false);
   };
 
   useEffect(() => {
@@ -45,7 +45,76 @@ function App() {
           <Main weatherData={weatherData} />
           <Footer />
         </div>
-        <ModalWithForm isModalOpen={isModalOpen} />
+        <ModalWithForm
+          activeModal={activeModal}
+          title="New garment"
+          buttonText="Add garment"
+        >
+          <fieldset className="form__fieldset">
+            <label htmlFor="name-input" className="form__label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form__input"
+              id="name-input"
+              placeholder="Name"
+              minLength="2"
+              maxLength="30"
+              required
+            />
+          </fieldset>
+          <fieldset className="form__fieldset">
+            <label htmlFor="image-input" className="form__label">
+              Image
+            </label>
+            <input
+              type="url"
+              className="form__input"
+              id="image-input"
+              placeholder="Image URL"
+              minLength="2"
+              maxLength="30"
+              required
+            />
+          </fieldset>
+          <fieldset className="form__fieldset form__fieldset_radio">
+            <legend className="form__legend">Select the weather type:</legend>
+            <label htmlFor="hot" className="form__label form__label-radio">
+              <input
+                type="radio"
+                className="form__input form__input_radio form__label-radio"
+                id="hot"
+                value="hot"
+                name="weather"
+                required
+              />
+              <span className="form__label-text">Hot</span>
+            </label>
+            <label htmlFor="warm" className="form__label form__label-radio">
+              <input
+                type="radio"
+                className="form__input form__input_radio"
+                id="warm"
+                value="warm"
+                name="weather"
+                required
+              />
+              <span className="form__label-text">Warm</span>
+            </label>
+            <label htmlFor="cold" className="form__label form__label-radio">
+              <input
+                type="radio"
+                className="form__input form__input_radio"
+                id="cold"
+                value="cold"
+                name="weather"
+                required
+              />
+              <span className="form__label-text">Cold</span>
+            </label>
+          </fieldset>
+        </ModalWithForm>
       </div>
     </>
   );
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,83 +1,24 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({ isModalOpen, handleModalClose }) {
+function ModalWithForm({
+  children,
+  buttonText,
+  title,
+  activeModal,
+  handleModalClose,
+}) {
   return (
-    <div className="form-modal" show={isModalOpen}>
+    <div className="form-modal" show={activeModal}>
       <div className="form-modal__container">
         <button
           type="button"
           className="form-modal__close-button"
           onClick={handleModalClose}
         ></button>
-        <h2 className="form-modal__title">title placeholder</h2>
-        <form className="form">
-          <fieldset className="form__fieldset">
-            <label htmlFor="name-input" className="form__label">
-              Name
-            </label>
-            <input
-              type="text"
-              className="form__input"
-              id="name-input"
-              placeholder="Name"
-              minLength="2"
-              maxLength="30"
-              required
-            />
-          </fieldset>
-          <fieldset className="form__fieldset">
-            <label htmlFor="image-input" className="form__label">
-              Image
-            </label>
-            <input
-              type="url"
-              className="form__input"
-              id="image-input"
-              placeholder="Image URL"
-              minLength="2"
-              maxLength="30"
-              required
-            />
-          </fieldset>
-          <fieldset className="form__fieldset form__fieldset_radio">
-            <legend className="form__legend">Select the weather type:</legend>
-            <label htmlFor="hot" className="form__label form__label-radio">
-              <input
-                type="radio"
-                className="form__input form__input_radio form__label-radio"
-                id="hot"
-                value="hot"
-                name="weather"
-                required
-              />
-              <span className="form__label-text">Hot</span>
-            </label>
-            <label htmlFor="warm" className="form__label form__label-radio">
-              <input
-                type="radio"
-                className="form__input form__input_radio"
-                id="warm"
-                value="warm"
-                name="weather"
-                required
-              />
-              <span className="form__label-text">Warm</span>
-            </label>
-            <label htmlFor="cold" className="form__label form__label-radio">
-              <input
-                type="radio"
-                className="form__input form__input_radio"
-                id="cold"
-                value="cold"
-                name="weather"
-                required
-              />
-              <span className="form__label-text">Cold</span>
-            </label>
-          </fieldset>
-        </form>
+        <h2 className="form-modal__title">{title}</h2>
+        <form className="form">{children}</form>
         <button type="submit" className="form-modal__button">
-          save placeholder
+          {buttonText}
         </button>
       </div>
     </div>
