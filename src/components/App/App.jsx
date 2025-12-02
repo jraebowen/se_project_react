db39bfd9045352611b4b0927178081790075332b
Jaimie Bowen <jraebowen@gmail.com>
2025-12-02 13:46:01 -0800

    add navigate import to app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 //react imports
 import { useState, useEffect, useCallback } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 //css import
 import "./App.css";
 //components
